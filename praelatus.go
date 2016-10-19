00a0f5967d249ba004897401b28218561757c8e8
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-10-18 22:13:56 -0400

    rename tessera to praelatus.go

--- praelatus.go
+++ praelatus.go
@@ -6,17 +6,11 @@ import (
 
 	"github.com/Sirupsen/logrus"
 	"github.com/praelatus/backend/api"
-	"github.com/praelatus/backend/utils"
 )
 
 func main() {
 	fmt.Println("Starting Praelatus!")
 	fmt.Println("Initializing database...")
-	db, err := utils.InitDB(os.Getenv("PRAELATUS_DB_URL"))
-	if err != nil {
-		fmt.Println("Failed to connect to database", err)
-		os.Exit(1)
-	}
 
 	logger := logrus.New()
 	api := api.New(&logger)
