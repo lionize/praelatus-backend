826c510e65e975ae25a5a63a42da8227c3f84b3b
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-10-23 19:41:09 -0400

    convert from labstack to new context

--- api/tickets.go
+++ api/tickets.go
@@ -1,73 +1,81 @@
 package api
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/praelatus/backend/models"
-	"github.com/labstack/echo"
 )
 
 func InitTicketRoutes() {
-
+	BaseRoutes.Tickets.Handle("/{team_slug}/{pkey}/{key}", NoAuth(GetTicket, false, false)).Methods("GET")
+	BaseRoutes.Tickets.Handle("/{team_slug}/{pkey}/{key}", NoAuth(CreateTicket, false, false)).Methods("POST")
+	BaseRoutes.Tickets.Handle("/{team_slug}/{pkey}/{key}", NoAuth(UpdateTicket, false, false)).Methods("PUT")
 }
 
 // TODO: Fix error handling
 
 // ListTickets will list all tickets in the database
-func ListTickets(c *Context) error {
-	var tickets []models.Ticket
+func ListTickets(c *Context) (int, []byte) {
+	var tickets []models.TicketJSON
 
-	tickets := store.Tickets().GetAll()
-	jsn, err := json.Marshal(tickets)
+	tickets = store.Tickets().GetAll(c.String("team_slug"), c.String("pkey"))
+
+	tjson, err := json.Marshal(tickets)
 	if err != nil {
-		return (http.StatusInternalServerError)
+		return http.StatusInternalServerError, []byte(err.Error())
 	}
-	return c.JSON(http.StatusOK, tickets)
+
+	return http.StatusOK, tjson
 }
 
 // CreateTicket creates a ticket
-func (gc *GlobalContext) CreateTicket(c echo.Context) error {
-	var t models.Ticket
-	c.Bind(&t)
-
-	gc.db.Create(&t)
-	if err := gc.CheckDbError(); err != nil {
-		return c.String(http.StatusInternalServerError, err.Error())
+// TODO
+func CreateTicket(c *Context) (int, []byte) {
+	var t models.TicketJSON
+	err := json.Unmarshal(&t)
+	if err != nil {
+		return http.StatusBadRequest, []byte(err.Error())
 	}
 
-	return nil
+	err = Store.Tickets().Save(&t)
+	if err != nil {
+		return http.StatusInternalServerError, []byte(err.Error())
+	}
+
+	return http.StatusOK, []byte("Ticket successfully created.")
 }
 
 // UpdateTicket will update the ticket at :key
-// TODO: implement this, updating will suck if it needs to be performant
-func (gc *GlobalContext) UpdateTicket(c echo.Context) error {
-	var t models.Ticket
-	c.Bind(&t)
-
-	return nil
+// TODO
+func UpdateTicket(c *Context) (int, []byte) {
+	return http.StatusNotImplemented, []byte("Not implemented")
 }
 
 // GetTicket will get a specific ticket
-func (gc *GlobalContext) GetTicket(c echo.Context) error {
-	var t models.Ticket
-	key := c.Param("key")
+func GetTicket(c *Context) error {
+	key := c.Vars.String("key")
 
-	gc.db.Where("key = ?", key).First(&t)
-	if err := gc.CheckDbError(); err != nil {
-		return c.String(http.StatusInternalServerError, err.Error())
+	t, err := Store.Tickets().Get(key)
+	if err != nil {
+		return http.StatusNotFound, []byte(err.Error())
 	}
 
-	return c.JSON(http.StatusOK, &t)
+	tjson, err := json.Marshal(&t)
+	if err != nil {
+		return http.StatusInternalServerError, []byte(err.Error())
+	}
+
+	return http.StatusOK, &t
 }
 
 // TODO: Implement workflows
-
 // AdvanceTicket advances a ticket through it's workflow.
-func (gc *GlobalContext) AdvanceTicket(c echo.Context) error {
-	return nil
+func AdvanceTicket(c *Context) (int, []byte) {
+	return http.StatusNotImplemented, []byte("Not implemented")
 }
 
 // RetractTicket will move a ticket backwards in it's workflow
-func (gc *GlobalContext) RetractTicket(c echo.Context) error {
-	return nil
+func RetractTicket(c *Context) (int, []byte) {
+	return http.StatusNotImplemented, []byte("Not implemented")
 }
