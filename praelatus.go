7cef441ae50eb692795a68e70575271491c5f1a3
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-10-22 21:25:58 -0400

    try different logging

--- praelatus.go
+++ praelatus.go
@@ -2,18 +2,22 @@ package main
 
 import (
 	"fmt"
+	"io"
 	"os"
 
-	"github.com/Sirupsen/logrus"
 	"github.com/praelatus/backend/api"
+
+	log "github.com/golang/glog"
 )
 
 func main() {
 	fmt.Println("Starting Praelatus!")
 	fmt.Println("Initializing database...")
 
-	logger := logrus.New()
-	api := api.New(&logger)
+	mw := io.MultiWriter(os.Stdout)
+	log.Info("Hello!")
+
+	api := api.New()
 
 	logger.Info("Ready to serve requests!")
 	api.Run(":3001")
