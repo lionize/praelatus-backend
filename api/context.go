7a8cf03a280da4751a7fc824679811eab02107dd
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-10-23 19:40:26 -0400

    add GoDoc comments to context

--- api/context.go
+++ api/context.go
@@ -1,39 +1,71 @@
 package api
 
 import (
-	"encoding/json"
 	"net/http"
+	"strings"
 	"time"
 
-	"github.com/praelatus/backend/models"
 	"github.com/gorilla/mux"
+	"github.com/praelatus/backend/models"
 )
 
+// Context will hold the vars from the request and has a special field for
+// holding the currently logged in user.
 type Context struct {
 	CurrentUser *models.User
-	Vars        map[string]interface{}
+	Vars        *Vars
 }
 
-func (c *Context) Unauthenticated() bool {
-	if c.CurrentUser == nil {
+// Authenticated will return a boolean indicating whether or not CurrentUser is
+// nil or not.
+func (c *Context) Authenticated() bool {
+	if c.CurrentUser != nil {
 		return true
 	}
 
 	return false
 }
 
+type Vars struct {
+	internal map[string]interface{}
+}
+
+func (v *Vars) String(key string) string {
+	vl, ok := v.internal[key].(string)
+	if ok {
+		return vl
+	}
+
+	return ""
+}
+
+func NewVars(m map[string]interface{}) *Vars {
+	return &Vars{
+		internal: m,
+	}
+}
+
+// AppHandler is a custom function type which handles a http request, it takes
+// a pointer to our context as the first argument and returns an integer and
+// []byte which will be the response status code and body respectively.
 type AppHandler func(*Context) (int, []byte)
 
+// AdminRequired takes an AppHandler and returns a http.Handler which will
+// check if the jwt token provided belongs to a user with admin rights.
 func AdminRequired(fn AppHandler) http.Handler {
 	return &handler{fn, true, true}
 }
 
+// AuthRequired takes an AppHandler and returns a http.Handler which will
+// required that a user is logged in before executing the request.
 func AuthRequired(fn AppHandler) http.Handler {
-    return &handler{fn, true, false}
+	return &handler{fn, true, false}
 }
 
+// NoAuth takes an AppHandler and returns a http.Handler which requires no
+// authentication before executing.
 func NoAuth(fn AppHandler) http.Handler {
-    return &handler{fn, false, false}
+	return &handler{fn, false, false}
 }
 
 type handler struct {
@@ -42,24 +74,23 @@ type handler struct {
 	reqAdmin bool
 }
 
+// ServeHTTP allows our handler struct to implement the http.Handler interface.
 func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-    var token string
+	var token string
 	start := time.Now()
 
 	c := &Context{}
-	c.Vars = mux.Vars(r)
+	c.Vars = NewVars(mux.Vars(r))
 
-    // Attempt to parse token out of the header
-    authHeader := r.Header.Get("Authorization")
-    if len(authHeader) > 6 && strings.ToUpper(authHeader[0:6]) == "BEARER" {
-        // Default session token
-        token = authHeader[7:]
-    } else if len(authHeader) > 5 && strings.ToLower(authHeader[0:5]) == "token" {
-        // OAuth token
-        token = authHeader[6:]
-    }
-
-	token, _ := json.Unmarshal(Srv.Cache.Get(token), c.CurrentUser)
+	// Attempt to parse token out of the header
+	authHeader := r.Header.Get("Authorization")
+	if len(authHeader) > 6 && strings.ToUpper(authHeader[0:6]) == "BEARER" {
+		// Default session token
+		tokenStr = authHeader[7:]
+	} else if len(authHeader) > 5 && strings.ToLower(authHeader[0:5]) == "token" {
+		// OAuth token
+		tokenStr = authHeader[6:]
+	}
 
 	// Commented out for now to make development less of a headache.
 	// 	if c.CurrentUser == nil && h.reqUser {
@@ -73,8 +104,8 @@ func (h *handler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// 	}
 
 	statusCode, response := h.fn(c)
-	Log.Infof("|%s| [%d] %s %s", 
-              r.Method, statusCode, r.URL.Path, time.Since(start).String())
+	Log.Infof("|%s| [%d] %s %s",
+		r.Method, statusCode, r.URL.Path, time.Since(start).String())
 
 	w.WriteHeader(statusCode)
 	_, err := w.Write(response)
