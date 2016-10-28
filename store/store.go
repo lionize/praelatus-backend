becf08eab7499a1ee81427636bc2f4b04100c5e2
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-10-28 13:14:50 -0400

    update stubs

--- store/store.go
+++ store/store.go
@@ -10,9 +10,13 @@ import (
 // Store is an interface for storing and retrieving models.
 type Store interface {
 	Users() UserStore
-	Projects() ProjectStore
-	Tickets() TicketStore
+	Teams() TeamStore
 	Fields() FieldStore
+	Tickets() TicketStore
+	Projects() ProjectStore
+	Statuses() StatusStore
+	Workflows() WorkflowStore
+	Transitions() TransitionStore
 }
 
 // SQLStore is used where you need extra methods for dealing with a sql backend.
@@ -31,7 +35,7 @@ type Cache interface {
 // FieldStore contains methods for storing and retrieving Fields and
 // FieldValues
 type FieldStore interface {
-	Get(id int) (*models.Field, error)
+	Get(int) (*models.Field, error)
 	ByProject(int) (*models.Field, error)
 	Save(*models.Field) error
 }
@@ -47,7 +51,7 @@ type UserStore interface {
 
 // ProjectStore contains methods for storing and retrieving Projects
 type ProjectStore interface {
-	Get(int) *models.Project
+	Get(int) (*models.Project, error)
 	GetAll() []models.Project
 	New(*models.Project) (*models.Project, error)
 	Save(*models.Project) error
@@ -60,13 +64,22 @@ type TicketStore interface {
 }
 
 // TeamStore contains methods for storing and retrieving Teams
-type TeamStore interface{}
+type TeamStore interface {
+	Get(int) (*models.Team, error)
+	GetBySlug(string) (*models.Team, error)
+}
 
 // StatusStore contains methods for storing and retrieving Statuses
-type StatusStore interface{}
+type StatusStore interface {
+	Get(int) (*models.Status, error)
+}
 
 // WorkflowStore contains methods for storing and retrieving Workflows
-type WorkflowStore interface{}
+type WorkflowStore interface {
+	Get(int) (*models.Workflow, error)
+}
 
 // TransitionStore contains methods for storing and retrieving Transitions
-type TransitionStore interface{}
+type TransitionStore interface {
+	Get(int) (*models.WorkflowTransition, error)
+}
