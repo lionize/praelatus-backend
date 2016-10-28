1ebb53ff62e338aed0e90dd96166dc66151cbca7
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-10-28 16:01:29 -0400

    godoc

--- store/pg/pg_workflow_store.go
+++ store/pg/pg_workflow_store.go
@@ -2,6 +2,8 @@ package pg
 
 import "github.com/jmoiron/sqlx"
 
+// WorkflowStore contains methods for saving/retrieving workflows from a
+// postgres DB
 type WorkflowStore struct {
 	db *sqlx.DB
 }
