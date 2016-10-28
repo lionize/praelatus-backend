34cb30f447d360488671d6104a94b14ebd6fc651
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-10-28 15:59:21 -0400

    finish fieldstore

--- store/pg/pg_field_store.go
+++ store/pg/pg_field_store.go
@@ -10,5 +10,74 @@ type FieldStore struct {
 }
 
 func (f *FieldStore) Get(id int) (*models.Field, error) {
-	return nil, nil
+	var field models.Field
+	err := f.db.QueryRowx("SELECT * FROM fields WHERE id = $1;", id).
+		StructScan(&field)
+	return &field, nil
+}
+
+func (f *FieldStore) GetByProject(projectID int) ([]models.Field, error) {
+	var fields []models.Field
+
+	rows, err := f.db.Queryx("SELECT * FROM fields WHERE project_id = $1", projectID)
+	if err != nil {
+		return fields, err
+	}
+
+	for rows.Next() {
+		var field models.Field
+
+		err = rows.StructScan(&field)
+		if err != nil {
+			return fields, err
+		}
+
+		fields = append(fields, field)
+	}
+
+	return fields, nil
+}
+
+func (f *FieldStore) GetAll() ([]models.Field, error) {
+	var fields []models.Field
+
+	rows, err := f.db.Queryx("SELECT * FROM fields;")
+	if err != nil {
+		return fields, err
+	}
+
+	for rows.Next() {
+		var field models.Field
+
+		err = rows.StructScan(&field)
+		if err != nil {
+			return fields, err
+		}
+
+		fields = append(fields, field)
+	}
+
+	return fields, nil
+}
+
+func (f *FieldStore) GetByProject(projectID int) ([]models.Field, error) {
+	var fields []models.Field
+
+	rows, err := f.db.Queryx("SELECT * FROM fields WHERE project_id = ?;", projectID)
+	if err != nil {
+		return fields, err
+	}
+
+	for rows.Next() {
+		var field models.Field
+
+		err = rows.StructScan(&field)
+		if err != nil {
+			return fields, err
+		}
+
+		fields = append(fields, field)
+	}
+
+	return fields, nil
 }
