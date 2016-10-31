c3e370fe8106b91adee963f9a8a896eea4a82979
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-10-31 15:57:17 -0400

    stubbed out and godoc

--- store/pg/pg_ticket_store.go
+++ store/pg/pg_ticket_store.go
@@ -1,9 +1,32 @@
 package pg
 
-import "github.com/jmoiron/sqlx"
+import (
+	"github.com/jmoiron/sqlx"
+	"github.com/praelatus/backend/models"
+)
 
+// TicketStore contains methods for storing and retrieving Tickets from
+// Postgres DB
 type TicketStore struct {
 	db *sqlx.DB
 }
 
-// TODO implement interfaces
+// Get TODO
+func (ts *TicketStore) Get(ID int) (*models.Ticket, error) {
+	return nil, nil
+}
+
+// GetByKey TODO
+func (ts *TicketStore) GetByKey(teamSlug string, projectKey string, ticketKey string) (*models.Ticket, error) {
+	return nil, nil
+}
+
+// Save TODO
+func (ts *TicketStore) Save(ticket *models.Ticket) error {
+	return nil
+}
+
+// New TODO
+func (ts *TicketStore) New(ticket *models.Ticket) error {
+	return nil
+}
