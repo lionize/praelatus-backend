75864d0f7986f5f99b07ca3c68f64a9a73b70018
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-11-01 20:49:35 -0400

    finished comment model

--- models/comment.go
+++ models/comment.go
@@ -2,13 +2,16 @@ package models
 
 // Comment is a comment on an issue / ticket.
 type Comment struct {
-	ID       uint
-	Body     string `json:"body"`
-	TicketID uint   `json:"-"`
+	ID   int64  `json:"id" db:"id"`
+	Body string `json:"body" db:"body"`
+
+	TicketID int64 `json:"-" db:"ticket_id"`
+	AuthorID int64 `json:"-" db:"author_id"`
 }
 
 // CommentJSON is a struct for JSON serialization
 type CommentJSON struct {
 	Comment
+
 	Author User `json:"author"`
 }
