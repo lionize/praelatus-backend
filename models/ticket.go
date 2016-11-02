341b0e134ea4e7dbce98d10534a56287870b8055
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-11-01 20:51:26 -0400

    finished ticket ticketype and status models

--- models/ticket.go
+++ models/ticket.go
@@ -4,18 +4,18 @@ import "time"
 
 // TicketType represents the type of ticket.
 type TicketType struct {
-	ID   int64  `json:"id"`
-	Name string `json:"name"`
+	ID   int64  `json:"id" db:"id"`
+	Name string `json:"name" db:"name"`
 }
 
 // Ticket represents a ticket in the database.
 type Ticket struct {
-	ID          int64     `json:"id"`
-	Key         string    `json:"key"`
-	Summary     string    `json:"summary"`
-	Description string    `json:"description"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
+	ID          int64     `json:"id" db:"id"`
+	Key         string    `json:"key" db:"key"`
+	Summary     string    `json:"summary" db:"key"`
+	Description string    `json:"description" db:"description"`
+	CreatedAt   time.Time `json:"created_at" db:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at" db:"created_at"`
 
 	ProjectID    int64 `json:"-" db:"project_id"`
 	TicketTypeID int64 `json:"-" db:"ticket_type_id"`
@@ -36,6 +36,6 @@ type TicketJSON struct {
 
 // Status represents a ticket's current status.
 type Status struct {
-	ID   int64  `json:"id"`
-	Name string `json:"name"`
+	ID   int64  `json:"id" db:"id"`
+	Name string `json:"name" db:"name"`
 }
