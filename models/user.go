9e82ed1980165d4d08d24a3d29d612f0519087c3
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-11-01 20:51:41 -0400

    finished user struct

--- models/user.go
+++ models/user.go
@@ -1,19 +1,24 @@
 package models
 
 import (
+	"crypto/md5"
+	"encoding/hex"
 	"fmt"
+	"strings"
 
 	"golang.org/x/crypto/bcrypt"
 )
 
 // User represents a user of our application
 type User struct {
-	ID       int64  `json:"id"`
-	Username string `json:"username"`
-	Password string `json:"password,omitempty"`
-	Email    string `json:"email"`
-	FullName string `json:"full_name" db:"full_name"`
-	IsAdmin  bool   `json:"is_admin,omitempty" db:"is_admin"`
+	ID         int64  `json:"id" db:"id"`
+	Username   string `json:"username" db:"username"`
+	Password   string `json:"password,omitempty" db:"password"`
+	Email      string `json:"email" db:"email"`
+	FullName   string `json:"full_name" db:"full_name"`
+	Gravatar   string `json:"gravatar" db:"gravatar"`
+	ProfilePic string `json:"profile_pic" db:"profile_pic"`
+	IsAdmin    bool   `json:"is_admin,omitempty" db:"is_admin"`
 }
 
 func (u *User) String() string {
@@ -27,11 +32,16 @@ func NewUser(username, password, fullName, email string, admin bool) (*User, err
 		return &User{}, err
 	}
 
+	emailHash := md5.Sum([]byte(strings.ToLower(email)))
+	eh := hex.EncodeToString(emailHash[:16])
+
 	return &User{
-		Username: username,
-		Password: string(pw),
-		Email:    email,
-		FullName: fullName,
-		IsAdmin:  admin,
+		Username:   username,
+		Password:   string(pw),
+		Email:      email,
+		FullName:   fullName,
+		ProfilePic: "https://www.gravatar.com/avatar/" + eh,
+		Gravatar:   "https://www.gravatar.com/avatar/" + eh,
+		IsAdmin:    admin,
 	}, nil
 }
