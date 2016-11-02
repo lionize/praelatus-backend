64b354e051c7ca3c6be5fc64b6827e57a0c57bf2
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-11-01 20:49:46 -0400

    finished field and fieldvalue models

--- models/field.go
+++ models/field.go
@@ -1,7 +1,36 @@
 package models
 
+import "encoding/json"
+
+// DT is a string representing the kind of data type that a field takes
+type DT string
+
+const (
+	// FloatT is used to emulate an enum for the DataType FLOAT type
+	FloatT DT = "FLOAT"
+
+	// StringT is used to emulate an enum for the DataType FLOAT type
+	StringT = "STRING"
+
+	// IntegerT is used to emulate an enum for the DataType FLOAT type
+	IntegerT = "INT"
+
+	// DateT is used to emulate an enum for the DataType FLOAT type
+	DateT = "DATE"
+)
+
 // Field is a ticket field
-type Field struct{}
+type Field struct {
+	ID       int64  `json:"id" db:"id"`
+	Name     string `json:"name" db:"name"`
+	DataType DT     `json:"data_type" db:"data_type"`
+}
 
 // FieldValue holds the value for a field on a given ticket.
-type FieldValue struct{}
+type FieldValue struct {
+	FieldID  int64 `json:"-" db:"field_id"`
+	TicketID int64 `json:"-" db:"ticket_id"`
+
+	// Value holds the raw JSONB from the db
+	Value json.RawMessage `db:"value"`
+}
