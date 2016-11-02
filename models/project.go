0cc5f90d60b4cdfbb18bc3282cf55480ffd9af9b
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-11-02 11:41:25 -0400

    added missing foreign keys to project model

--- models/project.go
+++ models/project.go
@@ -12,7 +12,7 @@ const (
 	UserR  = "USER"
 )
 
-// Project is the model used to represent a project in Tessera
+// Project is the model used to represent a project in the database.
 type Project struct {
 	ID          int64     `json:"id" db:"id"`
 	CreatedDate time.Time `json:"created_date" db:"created_date"`
@@ -20,6 +20,9 @@ type Project struct {
 	Name        string    `json:"name" db:"name"`
 	Key         string    `json:"key" db:"key"`
 	GithubRepo  string    `json:"github_repo,omitempty" db:"github_repo"`
+
+	LeadID int64 `json:"-" db:"lead_id"`
+	TeamID int64 `json:"-" db:"team_id"`
 }
 
 // Permission is used to control user access to teams and projects.
