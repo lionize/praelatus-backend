f6f9b06a2483ce71fccedae20b7c4f90e928bcdc
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-11-03 08:06:26 -0400

    added logging

--- store/pg/migrations/migrations.go
+++ store/pg/migrations/migrations.go
@@ -1,6 +1,9 @@
 package migrations
 
-import "github.com/jmoiron/sqlx"
+import (
+	log "github.com/iamthemuffinman/logsip"
+	"github.com/jmoiron/sqlx"
+)
 
 type schema struct {
 	v int
@@ -28,10 +31,14 @@ func SchemaVersion(db *sqlx.DB) int {
 // RunMigrations will run all database migrations depending on the version
 // returned from the database_information table.
 func RunMigrations(db *sqlx.DB) error {
+	version := SchemaVersion(db)
+	log.Infof("Current database version %d\n", version)
+
 	for _, schema := range schemas {
-		version := SchemaVersion(db)
+		version = SchemaVersion(db)
 
 		if version < schema.v {
+			log.Infof("Migrating database to version %d\n", schema.v)
 			_, err := db.Exec(schema.q)
 			if err != nil {
 				return err
