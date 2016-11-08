c20b76c8a365f9c9a4961c276defbffe216f17af
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-11-08 15:45:28 -0500

    update db tags

--- models/ticket.go
+++ models/ticket.go
@@ -11,11 +11,11 @@ type TicketType struct {
 // Ticket represents a ticket in the database.
 type Ticket struct {
 	ID          int64     `json:"id" db:"id"`
+	CreatedDate time.Time `json:"created_date" db:"created_date"`
+	UpdatedDate time.Time `json:"updated_date" db:"updated_date"`
 	Key         string    `json:"key" db:"key"`
-	Summary     string    `json:"summary" db:"key"`
+	Summary     string    `json:"summary" db:"summary"`
 	Description string    `json:"description" db:"description"`
-	CreatedAt   time.Time `json:"created_at" db:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at" db:"created_at"`
 
 	ProjectID    int64 `json:"-" db:"project_id"`
 	TicketTypeID int64 `json:"-" db:"ticket_type_id"`
