de37abb0de3a82b3fb7a1f7be9a28160ebaf599e
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-11-09 20:34:03 -0500

    wrote label store tests

--- store/pg/label_store_test.go
+++ store/pg/label_store_test.go
@@ -1,9 +1,46 @@
-package pg
+package pg_test
 
 import "testing"
 
-// TODO
-func TestGet(t *testing.T) {
+func TestLabelGet(t *testing.T) {
 	l, e := s.Labels().Get(1)
-	failIfErr(t, e)
+	failIfErr("Label Get", t, e)
+
+	if l == nil {
+		t.Error("Expected a label and got nil instead.")
+	}
+
+	if l.Name == "" {
+		t.Errorf("Expected label to have name got %s\n", l.Name)
+	}
+}
+
+func TestLabelGetAll(t *testing.T) {
+	l, e := s.Labels().GetAll()
+	failIfErr("Label Get All", t, e)
+
+	if l == nil {
+		t.Error("Expected to get some labels and got nil instead.")
+	}
+
+	if len(l) == 0 {
+		t.Errorf("Expected labels to be returned got %d labels instead\n", len(l))
+	}
+}
+
+func TestLabelSave(t *testing.T) {
+	l, e := s.Labels().Get(1)
+	failIfErr("Label Save", t, e)
+
+	l.Name = "SAVE TEST LABEL"
+
+	e = s.Labels().Save(l)
+	failIfErr("Label Save", t, e)
+
+	l, e = s.Labels().Get(1)
+	failIfErr("Label Save", t, e)
+
+	if l.Name != "SAVE TEST LABEL" {
+		t.Errorf("Expected: SAVE TEST LABEL Got: %s\n", l.Name)
+	}
 }
