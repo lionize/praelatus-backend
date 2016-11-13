203f821b2566986cb18c0029366a3e7aa7fe9d55
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-11-12 21:22:45 -0500

    make model less database dependent

--- models/comment.go
+++ models/comment.go
@@ -1,17 +1,19 @@
 package models
 
-// Comment is a comment on an issue / ticket.
-type Comment struct {
-	ID   int64  `json:"id" db:"id"`
-	Body string `json:"body" db:"body"`
+import "encoding/json"
 
-	TicketID int64 `json:"-" db:"ticket_id"`
-	AuthorID int64 `json:"-" db:"author_id"`
+func jsonString(i interface{}) string {
+	b, e := json.Marshal(i)
+	if e != nil {
+		return ""
+	}
+
+	return string(b)
 }
 
-// CommentJSON is a struct for JSON serialization
-type CommentJSON struct {
-	Comment
-
-	Author User `json:"author"`
+// Comment is a comment on a ticket.
+type Comment struct {
+	ID     int64  `json:"id"`
+	Body   string `json:"body"`
+	Author User   `json:"author"`
 }
