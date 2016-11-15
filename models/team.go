e56c249cfc07c024b9e31b3f89dbad98e39abd15
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-11-15 16:14:11 -0500

    change teams to just groups of users

--- models/team.go
+++ models/team.go
@@ -1,30 +1,13 @@
 package models
 
-import "strings"
-
 // Team maps directly to the teams database table.
 type Team struct {
-	ID       int64  `json:"id"`
-	Name     string `json:"name"`
-	URLSlug  string `json:"url_slug"`
-	Homepage string `json:"homepage"`
-	IconURL  string `json:"icon_url"`
-	Lead     User   `json:"lead"`
+	ID      int64  `json:"id"`
+	Name    string `json:"name"`
+	Lead    User   `json:"lead"`
+	Members []User `json:"members,omitempty"`
 }
 
 func (t *Team) String() string {
 	return jsonString(t)
-}
-
-// NewTeam makes a new team generating the slug based on it's name.
-func NewTeam(name, homepage, iconURL string) Team {
-	slug := strings.ToLower(name)
-	slug = strings.Replace(slug, " ", "-", -1)
-
-	return Team{
-		Name:     name,
-		IconURL:  iconURL,
-		Homepage: homepage,
-		URLSlug:  slug,
-	}
 }
