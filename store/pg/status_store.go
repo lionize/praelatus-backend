d42929019270f9c18e0092e026a86e740b2b7ffe
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-11-15 16:15:51 -0500

    implement new interface

--- store/pg/status_store.go
+++ store/pg/status_store.go
@@ -13,27 +13,32 @@ type StatusStore struct {
 }
 
 // Get gets a Status by it's ID in a postgres DB
-func (ss *StatusStore) Get(ID int64) (*models.Status, error) {
-	var s models.Status
-	err := ss.db.QueryRowx("SELECT * FROM statuses WHERE id = $1;", ID).
-		StructScan(&s)
-	return &s, handlePqErr(err)
+func (ss *StatusStore) Get(s *models.Status) error {
+	var row *sql.Row
+
+	row = ss.db.QueryRow(`SELECT id, name 
+						  FROM statuses 
+						  WHERE id = $1
+						  OR name = $2`, s.ID, s.Name)
+
+	err := row.Scan(&s.ID, &s.Name)
+	return handlePqErr(err)
 }
 
 // GetAll gets all the labess from the database
 func (ss *StatusStore) GetAll() ([]models.Status, error) {
 	var statuses []models.Status
-	rows, err := ss.db.Queryx("SELECT * FROM statuses;")
+	rows, err := ss.db.Query("SELECT * FROM statuses;")
 
 	for rows.Next() {
-		var l models.Status
+		var s models.Status
 
-		err := rows.StructScan(&l)
+		err := rows.Scan(&s.ID, &s.Name)
 		if err != nil {
 			return statuses, handlePqErr(err)
 		}
 
-		statuses = append(statuses, l)
+		statuses = append(statuses, s)
 	}
 
 	return statuses, handlePqErr(err)
@@ -50,7 +55,7 @@ func (ss *StatusStore) New(status *models.Status) error {
 }
 
 // Save updates a Status in the postgres DB
-func (ss *StatusStore) Save(status *models.Status) error {
+func (ss *StatusStore) Save(status models.Status) error {
 	_, err := ss.db.Exec(`UPDATE statuses SET (name) = ($1);`, status.Name)
 	return handlePqErr(err)
 }
