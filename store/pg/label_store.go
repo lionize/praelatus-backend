bfe11d057903967b5505f7d64c899f25e518719a
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-11-15 16:14:54 -0500

    implement new interface

--- store/pg/label_store.go
+++ store/pg/label_store.go
@@ -12,23 +12,30 @@ type LabelStore struct {
 	db *sql.DB
 }
 
-// Get gets a label from the database by it's ID
-func (ls *LabelStore) Get(ID int64) (*models.Label, error) {
-	var l models.Label
-	err := ls.db.QueryRowx("SELECT * FROM labels WHERE id = $1", ID).
-		StructScan(&l)
-	return &l, handlePqErr(err)
+// Get gets a label from the database
+func (ls *LabelStore) Get(l *models.Label) error {
+	var row *sql.Row
+
+	switch l.Name {
+	case "":
+		row = ls.db.QueryRow("SELECT id, name FROM labels WHERE id = $1", l.ID)
+	default:
+		row = ls.db.QueryRow("SELECT id, name FROM labels WHERE name = $1", l.Name)
+	}
+
+	err := row.Scan(&l.ID, &l.Name)
+	return handlePqErr(err)
 }
 
 // GetAll gets all the labels from the database
 func (ls *LabelStore) GetAll() ([]models.Label, error) {
 	var labels []models.Label
-	rows, err := ls.db.Queryx("SELECT * FROM labels;")
+	rows, err := ls.db.Query("SELECT id, name FROM labels;")
 
 	for rows.Next() {
 		var l models.Label
 
-		err := rows.StructScan(&l)
+		err := rows.Scan(&l.ID, &l.Name)
 		if err != nil {
 			return labels, handlePqErr(err)
 		}
@@ -50,7 +57,7 @@ func (ls *LabelStore) New(label *models.Label) error {
 }
 
 // Save updates a label in the database
-func (ls *LabelStore) Save(label *models.Label) error {
+func (ls *LabelStore) Save(label models.Label) error {
 	_, err := ls.db.Exec(`UPDATE labels SET (name) = ($1) 
 						  WHERE id = $2;`, label.Name, label.ID)
 
