c658c34647c2224f4de377685c4c74367bbc5b38
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-11-15 16:15:36 -0500

    implement new interface

--- store/pg/project_store.go
+++ store/pg/project_store.go
@@ -2,6 +2,7 @@ package pg
 
 import (
 	"database/sql"
+	"encoding/json"
 
 	"github.com/praelatus/backend/models"
 )
@@ -12,67 +13,71 @@ type ProjectStore struct {
 	db *sql.DB
 }
 
+func IntoProject(row rowScanner, p *models.Project) error {
+	var lead models.User
+	var ljson json.RawMessage
+
+	err := row.Scan(&p.ID, &p.CreatedDate, &p.Name, &p.Key,
+		&p.Homepage, &p.IconURL, &p.Repo, &ljson)
+	if err != nil {
+		return err
+	}
+
+	err = json.Unmarshal(ljson, &lead)
+	p.Lead = lead
+
+	return err
+}
+
 // Get gets a project by it's ID in a postgres DB.
-func (ps *ProjectStore) Get(ID int64) (*models.Project, error) {
-	var p models.Project
-	err := ps.db.QueryRowx("SELECT * FROM projects WHERE id = $1;", ID).
-		StructScan(&p)
-	return &p, handlePqErr(err)
+func (ps *ProjectStore) Get(p *models.Project) error {
+	var row *sql.Row
+
+	switch p.Key {
+	case "":
+		row = ps.db.QueryRow(`SELECT id, created_date, name, 
+								   key, homepage, icon_url, repo,
+								   rows_to_json(lead.*)
+							FROM projects 
+							JOIN users AS lead ON lead.id = projects.lead_id
+							WHERE id = $1;`, p.ID)
+	default:
+		row = ps.db.QueryRow(`SELECT id, created_date, name, 
+								   key, homepage, icon_url, repo,
+								   rows_to_json(lead.*)
+							FROM projects 
+							JOIN users AS lead ON lead.id = projects.lead_id
+							WHERE key = $1;`, p.Key)
+
+	}
+
+	err := IntoProject(row, p)
+	return handlePqErr(err)
 }
 
 // GetByKey gets a project by it's project key
-func (ps *ProjectStore) GetByKey(slug, key string) (*models.Project, error) {
-	var p struct {
-		ID          int64           `json:"id" db:"id"`
-		CreatedDate time.Time       `json:"created_date" db:"created_date"`
-		Name        string          `json:"name" db:"name"`
-		Key         string          `json:"key" db:"key"`
-		Homepage    string          `json:"homepage" db:"homepage"`
-		IconURL     string          `json:"icon_url" db:"icon_url"`
-		Repo        string          `json:"repo,omitempty" db:"repo"`
-		Team        json.RawMessage `json:"team" db:"team"`
-
-		LeadID int64 `json:"-" db:"lead_id"`
-		TeamID int64 `json:"-" db:"team_id"`
-	}
-
-	err := ps.db.QueryRowx(`SELECT p.id, p.created_date, p.name, p.key, p.repo, 
-								   p.homepage, p.icon_url, p.lead_id, p.team_id,
-								   row_to_json(teams.*) as team
+func (ps *ProjectStore) GetByKey(team models.Team, p *models.Project) error {
+	row := ps.db.QueryRow(`SELECT p.id, p.created_date, p.name, 
+								  p.key, p.repo, p.homepage, 
+								  p.icon_url, p.lead_id, p.team_id,
+								  row_to_json(lead.*)
 						    FROM projects AS p
-							JOIN teams ON p.team_id = teams.id
-							WHERE p.key = $1
-							AND teams.url_slug = $2;`,
-		key, slug).
-		StructScan(&p)
+							JOIN users AS lead ON lead.id = p.lead_id
+							WHERE p.key = $1`, p.Key)
 
-	var t models.Team
-	e := json.Unmarshal(p.Team, &t)
-	if e != nil {
-		return nil, e
-	}
-
-	fmt.Println(t)
-
-	return &models.Project{
-		ID:          p.ID,
-		CreatedDate: p.CreatedDate,
-		Name:        p.Name,
-		Key:         p.Key,
-		Homepage:    p.Homepage,
-		IconURL:     p.IconURL,
-		Repo:        p.Repo,
-		Team:        t,
-		LeadID:      p.LeadID,
-		TeamID:      p.TeamID,
-	}, handlePqErr(err)
+	err := IntoProject(row, p)
+	return handlePqErr(err)
 }
 
 // GetAll returns all projects
 func (ps *ProjectStore) GetAll() ([]models.Project, error) {
 	var projects []models.Project
 
-	rows, err := ps.db.Queryx(`SELECT * FROM projects;`)
+	rows, err := ps.db.Query(`SELECT p.id, p.created_date, p.name, 
+								  p.key, p.repo, p.homepage, 
+								  p.icon_url, p.lead_id, p.team_id,
+								  row_to_json(lead.*)
+							  FROM projects;`)
 	if err != nil {
 		return projects, handlePqErr(err)
 	}
@@ -80,7 +85,7 @@ func (ps *ProjectStore) GetAll() ([]models.Project, error) {
 	for rows.Next() {
 		var p models.Project
 
-		err = rows.StructScan(&p)
+		err = IntoProject(rows, &p)
 		if err != nil {
 			return projects, handlePqErr(err)
 		}
@@ -94,26 +99,24 @@ func (ps *ProjectStore) GetAll() ([]models.Project, error) {
 // New creates a new Project in the database.
 func (ps *ProjectStore) New(project *models.Project) error {
 	err := ps.db.QueryRow(`INSERT INTO projects 
-						   (name, key, repo, homepage, icon_url, 
-						    team_id, lead_id) 
-						   VALUES ($1, $2, $3, $4, $5, $6, $7)
+						   (name, key, repo, homepage, icon_url, lead_id) 
+						   VALUES ($1, $2, $3, $4, $5, $6)
 						   RETURNING id;`,
 		project.Name, project.Key, project.Repo, project.Homepage,
-		project.IconURL, project.TeamID, project.LeadID).
+		project.IconURL, project.Lead.ID).
 		Scan(&project.ID)
 
 	return handlePqErr(err)
 }
 
 // Save updates a Project in the database.
-func (ps *ProjectStore) Save(project *models.Project) error {
+func (ps *ProjectStore) Save(project models.Project) error {
 	_, err := ps.db.Exec(`UPDATE projects SET
-						  (name, key, repo, homepage, icon_url, 
-						  team_id, lead_id) =
-						  ($1, $2, $3, $4, $5, $6, $7)
-						  WHERE id = $8;`,
+						  (name, key, repo, homepage, icon_url, lead_id) 
+						  = ($1, $2, $3, $4, $5, $6)
+						  WHERE id = $7;`,
 		project.Name, project.Key, project.Repo, project.Homepage,
-		project.IconURL, project.TeamID, project.LeadID, project.ID)
+		project.IconURL, project.Lead.ID, project.ID)
 
 	return handlePqErr(err)
 }
