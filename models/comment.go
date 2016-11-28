345ef63c4e5c666d63582b6aaab475e42fd226d6
Mathew Robinson <mathew.robinson3114@gmail.com>
2016-11-28 11:43:38 -0500

    add date fields to comments

--- models/comment.go
+++ models/comment.go
@@ -1,10 +1,14 @@
 package models
 
+import "time"
+
 // Comment is a comment on an issue / ticket.
 type Comment struct {
-	ID     int64  `json:"id"`
-	Body   string `json:"body"`
-	Author User   `json:"author"`
+	ID          int64     `json:"id"`
+	UpdatedDate time.Time `json:"updated_date"`
+	CreatedDate time.Time `json:"created_date"`
+	Body        string    `json:"body"`
+	Author      User      `json:"author"`
 }
 
 func (c *Comment) String() string {
